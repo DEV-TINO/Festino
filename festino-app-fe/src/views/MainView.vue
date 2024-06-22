3a9d4ca2090ffaff503a0b47941d2758bb125126
kim-song-jun <>
2024-06-22 21:06:39 +0900

    add : add font family and tailwind config

--- festino-app-fe/src/views/MainView.vue
+++ festino-app-fe/src/views/MainView.vue
@@ -1,7 +1,13 @@
 <script setup></script>
 
 <template>
-  <div class="w-40 h-40 bg-red-500">hello</div>
+  <div class="w-full h-40 bg-primary-700-light font-jalnan2">안녕하세요 hello !?., !@#$%^&*()_+=-</div>
+  <div class="w-full h-40 bg-primary-700 font-semibold text-4xl">안녕하세요 hello !?., !@#$%^&*()_+=-</div>
+  <div class="w-full h-40 bg-primary-700 font-bold text-4xl">안녕하세요 hello !?., !@#$%^&*()_+=-</div>
+  <div class="w-full h-40 bg-primary-500">안녕하세요 hello !?., !@#$%^&*()_+=-</div>
+  <div class="w-full h-40 bg-primary-300">안녕하세요 hello !?., !@#$%^&*()_+=-</div>
+  <div class="w-full h-40 bg-primary-100">안녕하세요 hello !?., !@#$%^&*()_+=-</div>
+  <div class="w-full h-40 bg-primary-50 text-primary-700">안녕하세요 hello !?., !@#$%^&*()_+=-</div>
 </template>
 
 <style lang="scss" scoped></style>
